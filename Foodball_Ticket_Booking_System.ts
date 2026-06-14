1f46fefb8499d9ea6c687c163369d116aa7a6a6b
Ramen Biswas <211234697+vercel-pro@users.noreply.github.com>
2026-06-15 00:11:38 +0600

    Update Foodball_Ticket_Booking_System.ts

--- Foodball_Ticket_Booking_System.ts
+++ Foodball_Ticket_Booking_System.ts
@@ -45,13 +45,14 @@ create table bookings(
       'Pending',
       'Confirmed',
       'Cancelled',
-      'Refunded'
+      'Refunded',
+      'Action Required'
     )
   ),
   total_cost int not null
 );
-drop table bookings
 
+ 
 -- Insert Data into User Table
 INSERT INTO users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
@@ -59,16 +60,17 @@ INSERT INTO users (user_id, full_name, email, role, phone_number) VALUES
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
--- Insert Data into User Table
+
+-- Insert Data into matches Table
 INSERT INTO matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
 (101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
 (102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
 (103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
-(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
-(106, 'Real Madrid vs Barcelona', 'Champions League', 110.00, 'Postponed'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available')
+  
 
--- Insert Data into User Table
+-- Insert Data into bookings Table
 INSERT INTO bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
 (501, 1, 101, 'A-12', 'Confirmed', 150.00),
 (502, 1, 102, 'B-04', 'Confirmed', 120.00),
@@ -76,22 +78,21 @@ INSERT INTO bookings (booking_id, user_id, match_id, seat_number, payment_status
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
+
+
+
+
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
-select * from matches where tournament_category = 'Champions League' and match_status = 'Available';
+select match_id, fixture,base_ticket_price from matches where tournament_category = 'Champions League' and match_status = 'Available';
+
 
 
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
-select * from users where full_name ilike 'tanvir%' or full_name ilike '%Haque%'
+select user_id, full_name, email from users where full_name ilike 'tanvir%' or full_name ilike '%Haque%'
 
+  
 
 -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
-  -- payment_status in (
-  --     'Pending',
-  --     'Confirmed',
-  --     'Cancelled',
-  --     'Refunded'
-  --   )
-select * from bookings where payment_status is null
 update bookings set payment_status = 'Action Required' where payment_status is null;
 
 
@@ -99,6 +100,7 @@ update bookings set payment_status = 'Action Required' where payment_status is n
 -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
 select booking_id, full_name, fixture, total_cost from bookings inner join users on bookings.user_id = users.user_id inner join matches on bookings.match_id = matches.match_id
 
+  
 
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
 select users.user_id, full_name, booking_id from users left join bookings on users.user_id = bookings.user_id
@@ -108,24 +110,11 @@ select users.user_id, full_name, booking_id from users left join bookings on use
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
 select booking_id, match_id, total_cost from bookings where total_cost > (select avg(total_cost) from bookings)
 
+  
 
 -- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
 select * from matches order by base_ticket_price desc offset 1 limit 2
-select * from matches where match_status != 'Postponed' order by base_ticket_price desc offset 1 limit 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- select * from matches where match_status != 'Postponed' order by base_ticket_price desc offset 1 limit 2
 
 
 
